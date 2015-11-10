cf844744083f60a0abc69fce1334a8e15b19ad62
Maorz30199 <cesarruiz@unicauca.edu.co>
2015-11-10 17:53:34 -0500

    Creación del puzzle
    primer puzzle para el 3 prototipo de enfasis, se recrea el tamplo 4 y al
    completar el puzzle se recrea el santuario, mostrando una imagen real
    del santuario

---
+++ Assets/Puzzle/Assets/Puzzle.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Puzzle : MonoBehaviour
+{
+    public bool mover;
+    public Transform slot;
+    float x;
+    float y;
+
+    //Variables verificar tag objeto
+    public GameObject script;
+    Aciertos modificar;
+    public string tagObjeto; // modificar los aciertos
+
+    //Mostrar imagen de gana
+
+    
+
+	void Start ()
+    {
+        mover = true;
+
+        tagObjeto = gameObject.tag;
+
+        modificar = script.GetComponent<Aciertos>();
+
+        
+
+        
+    }
+
+    void Update()
+    {
+        if(modificar.puntos == 8)
+        {
+            mover = false;
+            Application.LoadLevel("Templo4");          
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == tagObjeto)
+        {
+            modificar.puntos += 1;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == tagObjeto)
+        {
+            modificar.puntos -= 1;
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if(mover == true)
+        {
+            if(Vector3.Distance (transform.position, slot.position) == 1)
+            {
+                x = transform.position.x;
+                y = transform.position.y;
+
+                transform.position = new Vector3(slot.position.x, slot.position.y, 0);
+
+                slot.position = new Vector3(x, y, 0);
+            }
+
+        }
+    }
+
+
+}
