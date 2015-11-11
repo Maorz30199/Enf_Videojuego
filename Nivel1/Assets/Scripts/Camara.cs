f362e0e5f24714189fbccf8f8c108b80a563da72
Maorz30199 <cesarruiz@unicauca.edu.co>
2015-11-11 01:42:30 -0500

    Creación de runner inicial, con escenario y obstaculos
    parte del 3 prototipo de enfasis IV, con funcionalidades, integración
    con las demás escenas

---
+++ Nivel1/Assets/Scripts/Camara.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camara : MonoBehaviour
+{
+
+    
+    public Vector2 velocidad;
+    public float TiemY;
+    public float TiemX;
+
+    public GameObject personaje;
+
+	void Start ()
+    {
+        //personaje = GameObject.FindGameObjectWithTag("Personaje");
+	}
+	
+	
+	void Update ()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, personaje.transform.position.x, ref velocidad.x, TiemX);
+        float posY = Mathf.SmoothDamp(transform.position.y, personaje.transform.position.y, ref velocidad.y, TiemY);
+
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ Nivel1/Assets/Scripts/Movimientos.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimientos : MonoBehaviour
+{
+
+
+    public float maxSpeed = 3;
+    public float Velocidad = 50f;
+    public float Salto = 150f;
+
+    private Rigidbody2D rb2d;    
+
+
+    void Start ()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	void Update ()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up * Salto);
+        }
+	}
+
+    void FixedUpdate()
+    {
+    
+            float h = Input.GetAxis("Horizontal");
+            rb2d.AddForce((Vector2.right * Velocidad) * h);
+
+            if (rb2d.velocity.x > maxSpeed)
+            {
+                rb2d.velocity = new Vector2(maxSpeed, rb2d.velocity.y);
+            }   
+    }
+}
+
